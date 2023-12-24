3ad0584c1d6c2e5f7e47556eb00e58991066ba1a
shrimann23 <shrima.narasimhan@gmail.com>
2023-12-23 21:25:29 -0800

    added audio import

--- AudioScreen.js
+++ AudioScreen.js
@@ -1,35 +1,31 @@
 import React from 'react';
 import { View, Text, Button, Slider } from 'react-native';
-import Sound from 'react-native-sound';
+import Audio from 'react-native-audio';
 
 class AudioScreen extends React.Component {
     render() {
         const { navigation } = this.props;
 
-        const sound = new Sound('./assets/piano.mp3', (error) => {
-            if (error) {
-                console.log('Error loading audio file');
-            }
-        });
+        const audio = new Audio(require('./assets/piano.mp3'));
 
-        const playSound = () => {
-            sound.play();
-        };
+        // const playSound = () => {
+        //     audio.play();
+        // }
 
-        const pauseSound = () => {
-            sound.pause();
-        };
+        // const pauseSound = () => {
+        //     audio.pause();
+        // }
 
-        const stopSound = () => {
-            sound.stop();
-        }
+        // const stopSound = () => {
+        //     audio.stop();
+        // }
 
         return (
             <View>
                 <Text>Welcome to the audio page!</Text>
-                <Button title="Play" onPress={playSound} />
+                {/* <Button title="Play" onPress={playSound} />
                 <Button title="Pause" onPress={pauseSound} />
-                <Button title="Stop" onPress={stopSound} />
+                <Button title="Stop" onPress={stopSound} /> */}
             </View>
             
         );
